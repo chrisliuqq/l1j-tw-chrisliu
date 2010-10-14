8dc6afbb5e5daef961891be346270bdc65bab215
chrisliuqq <chrisliuqq@2292006a-f82b-11de-a831-516a40b15b2e>
2010-10-14 10:46:49 +0000

    補上幾個 Javadoc 的註解，順便測試 Eclipse 中的 SVN 功能

--- src/l1j/server/ClientThread.java
+++ src/l1j/server/ClientThread.java
@@ -8,6 +8,10 @@ import java.net.Socket;
  */
 public class ClientThread implements Runnable {
 
+	/**
+	 * 使用與 clinet 連線來建立執行序的建構子
+	 * @param socket 與 client 的連線
+	 */
 	public ClientThread(Socket socket) {
 		// TODO Auto-generated constructor stub
 	}
