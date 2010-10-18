9576aff932a8eca178425c8be6478ce7c9bf3d1b
chrisliuqq <chrisliuqq@2292006a-f82b-11de-a831-516a40b15b2e>
2010-10-18 15:29:57 +0000

    1. 修改了 ClientThread 與相關的部份 2. 原封不動的加入了 Blowfish 加解密的部份

---
+++ src/l1j/server/encryption/LineageKeys.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package l1j.server.encryption;
+
+/**
+ * @author ChrisLiu
+ */
+public class LineageKeys {
+	public long[] encodeKey = { 0, 0 };
+
+	public long[] decodeKey = { 0, 0 };
+}
--- src/l1j/server/serverpacket/S_InitialKey.java
+++ src/l1j/server/serverpacket/S_InitialKey.java
@@ -11,19 +11,29 @@ import l1j.Opcodes;
 // FIXME: ChrisLiu.2010/10/15: 這邊優先要改，尚未完成
 public class S_InitialKey extends ServerBasePacket {
 
+	private int seed = 0;
+
 	/**
 	 * 初始化金鑰的建構子
 	 * 
 	 * @return
 	 */
-	public byte[] S_InitialKey() {
+	public S_InitialKey() {
 		writeByte(Opcodes.S_OPCODE_INITIAL_KEY);
 		randomInt(); // 加解密種子
 		randomInt(); // 未知
 		randomInt(); // 未知
 		randomShort(); // 未知
 		randomByte(); // 未知
-		return getBytes();
+
+		seed |= getBytes()[1] << 0 & 0xff;
+		seed |= getBytes()[2] << 8 & 0xff00;
+		seed |= getBytes()[3] << 16 & 0xff0000;
+		seed |= getBytes()[4] << 24 & 0xff000000;
+	}
+
+	public int getKey() {
+		return seed;
 	}
 
 }
