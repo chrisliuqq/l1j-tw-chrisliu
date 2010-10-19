7fbad4486f5a99b046cda5f3ad737ef0372cc85e
chrisliuqq <chrisliuqq@2292006a-f82b-11de-a831-516a40b15b2e>
2010-10-19 18:59:57 +0000

    開始構築對地圖解析的架構，尚未實做。

---
+++ trunk/util/L1Tool/L1Tool/util/MapPackage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace L1Tool.util
+{
+	class MapPackage
+	{
+		private DirectoryInfo[] _mapFolderInfo;
+
+		public MapPackage(DirectoryInfo[] map)
+		{
+			this._mapFolderInfo = map;
+		}
+	}
+}
