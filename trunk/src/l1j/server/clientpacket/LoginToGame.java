1de6e14e5b98349bba635b5883790c5a38e5c3d4
chrisliuqq <chrisliuqq@2292006a-f82b-11de-a831-516a40b15b2e>
2010-10-24 16:22:23 +0000

    在實做登入與角色的一些屬性

---
+++ trunk/src/l1j/server/clientpacket/LoginToGame.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package l1j.server.clientpacket;
+
+import l1j.databases.CharacterTable;
+import l1j.server.ClientThread;
+import l1j.server.model.instance.L1PcInstance;
+import l1j.server.serverpacket.S_LoginGame;
+
+/**
+ * @author ChrisLiu
+ */
+public class LoginToGame extends ClientBasePacket {
+	public LoginToGame(byte[] data, ClientThread client) {
+		read(data);
+
+		// 取得角色名稱
+		String charName = readString();
+		// 登入帳號
+		String userName = client.getUsername();
+		// XXX: ChrisLiu.2010/10/24: L1PcInstance 尚未實做，整理一下架構之後在考慮資料庫要怎麼規劃
+		// 取得角色資料
+		L1PcInstance pc = CharacterTable.loadCharacter(charName);
+		// 設定上線狀態
+		pc.updateOnlineStatus(1);
+
+		pc.sendPacket(new S_LoginGame(1), false);
+		// UNKNOWN 的封包
+		pc.sendPacket(new S_LoginGame(2), false);
+	}
+}
